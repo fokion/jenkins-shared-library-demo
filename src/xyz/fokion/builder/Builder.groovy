ab08690ef41d34079d4125227a3e6b87dbe24330
Fokion Sotiropoulos <fokion.s@gmail.com>
2019-11-06 21:36:36 +0000

    annotate with @NonCPS  annotation
    https://wiki.jenkins.io/display/JENKINS/Pipeline+CPS+method+mismatches

--- src/xyz/fokion/builder/Builder.groovy
+++ src/xyz/fokion/builder/Builder.groovy
@@ -3,24 +3,24 @@ package xyz.fokion.builder
 class Builder {
     private String url
     private int port
-
+  
     Builder(String url) {
         this.url = url
     }
-
+    @NonCPS
     Builder setPort(int port) {
         this.port = port
         return this
     }
-
+    @NonCPS
     def getPort() {
         return port
     }
-
+    @NonCPS
     def getUrl() {
         return url
     }
-
+    @NonCPS
     DummyScanner build() {
         return new DummyScanner(this)
     }
